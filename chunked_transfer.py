597d98f01b2ca686623bb00ae32cc24b28e046d5
Houselyn <rhouselyn@gmail.com>
2023-12-11 16:54:27 +0800

    restructure

--- chunked_transfer.py
+++ chunked_transfer.py
@@ -3,13 +3,16 @@ class ChunkedTransfer:
         self.file_path = file_path
 
     def read_chunks(self, chunk_size=1024):
-        with open(self.file_path, 'rb') as file:
-            while True:
-                chunk = file.read(chunk_size)
-                if not chunk:
-                    break
+        try:
+            with open(self.file_path, "rb") as file:
+                while True:
+                    chunk = file.read(chunk_size)
+                    if not chunk:
+                        break
+                    yield chunk
+        except FileNotFoundError:
+            yield None
 
-                yield chunk
 
 if __name__ == "__main__":
     # 在这里可以添加一些测试代码
